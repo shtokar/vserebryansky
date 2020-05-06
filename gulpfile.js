6137d34d007c7b27cdbcdbb215f3cbfe6c846644
shtokar <vitaliy-2kar@yandex.ru>
2020-05-06 17:53:41 +0400

    feat: added plugin gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const browserSync = require('browser-sync').create();
 const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function bs() {
     serveSass();
@@ -18,6 +19,9 @@ function bs() {
 function serveSass() {
     return src("./sass/*.sass")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
