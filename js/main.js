8ab14dff9f2b6f5988a0a80b2999a9b281571a75
shtokar <vitaliy-2kar@yandex.ru>
2020-05-21 20:51:32 +0400

    feat: add button up-to-top and add jquery-scrypts

--- js/main.js
+++ js/main.js
@@ -40,10 +40,24 @@ document.addEventListener('keypress', function (e) {
   })
 $('#modal_id').modal({ backdrop: 'static', keyboard: false });
 
-/****************************************************************************************** */
+/********************************JQuery-code********************************************************* */
+/*(document).ready(function () {//
+    var modal = $('.modal'),//
+        modalBtn = $('[data-toggle=modal]'),//
+        closeBtn = $('.modal_close');//
 
-
-
+    modalBtn.on('click', function () {//
+        modal.toggleClass('modal--visible');//
+    });
+    closeBtn.on('click', function () {//
+        modal.toggleClass('modal--visible');//
+    });
+    $(document).on('click', function (e) {//
+        if (modal.is(e.target))
+            modal.toggleClass('modal--visible');
+    });
+/*+*/
+/******************************************************************************** 
 $(document).ready(function () {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
@@ -59,7 +73,82 @@ $(document).ready(function () {
         if (modal.is(e.target))
             modal.toggleClass('modal--visible');
     });
+****************************************************************************** 
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    next.css('left', prev.width() + 20 + bullets.width() + 20)
+    bullets.css('left', prev.width() + 20)
     
+});*/
+/*************
+function backToTop() {
+    let button = $('.back-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({scrollTop: 0}, 1000);
+    })
+}
+
+backToTop();
+
+*/
+/***********************************Функция при нажатии клавиши ************************************ */
+$( document ).ready(function(ESCclose){
+    $( "body" ).click(function(){ // задаем функцию при нажатиии на элемент button
+      $( "modal" ).keydown(); // вызываем событие keydown на элементе <div>
+    });
+    $( "modal" ).keydown(function(){ // задаем функцию при нажатиии любой клавиши клавиатуры на элементе
+      $( "modal--visible" ).css( "opacity", "0" ); // 
+      $( "modal--visible" ).css( "visibility", "hidden" ); // 
+
+    });
+  });
+/**************************Функция при нажатии клавиши выдает в консоли код клавиши************** */
+$( document ).ready(function(){
+    $( "body" ).keypress(function( event ){ // задаем функцию при нажатиии любой, кроме специальной клавиши клавиатуры на элементе
+      console.log( event.which ); // выводим код символа нажатой клавиши
+    });
+  });
+
+/***********************************cool scrypt***************************************** */
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    $(document).on('click', function (e) {
+        if (modal.is(e.target))
+            modal.toggleClass('modal--visible');
+    });
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         pagination: {
